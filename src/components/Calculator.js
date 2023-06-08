77717547e193fe75336c9fd22ea59b44d7713c4e
Nahid1911 <nahid.raihan@gmail.com>
2023-06-08 21:56:12 +0600

    Add logic to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-const TextArea = () => (
+const TextArea = ({ value, handleChange }) => (
   <div className="textAreaDiv">
-    <input type="text" />
+    <input type="text" value={value} onChange={handleChange} />
   </div>
 );
 
-const Numpad = () => (
+TextArea.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+const Numpad = ({ handleButtonClick }) => (
   <div className="numbDiv">
-    <button type="button">AC</button>
-    <button type="button">+/-</button>
-    <button type="button">%</button>
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button className="largeBtn" type="button">0</button>
+    <button type="button" onClick={() => handleButtonClick('AC')}>AC</button>
+    <button type="button" onClick={() => handleButtonClick('+/-')}>+/-</button>
+    <button type="button" onClick={() => handleButtonClick('%')}>%</button>
+    <button type="button" onClick={() => handleButtonClick('7')}>7</button>
+    <button type="button" onClick={() => handleButtonClick('8')}>8</button>
+    <button type="button" onClick={() => handleButtonClick('9')}>9</button>
+    <button type="button" onClick={() => handleButtonClick('4')}>4</button>
+    <button type="button" onClick={() => handleButtonClick('5')}>5</button>
+    <button type="button" onClick={() => handleButtonClick('6')}>6</button>
+    <button type="button" onClick={() => handleButtonClick('1')}>1</button>
+    <button type="button" onClick={() => handleButtonClick('2')}>2</button>
+    <button type="button" onClick={() => handleButtonClick('3')}>3</button>
+    <button className="largeBtn" type="button" onClick={() => handleButtonClick('0')}>0</button>
     <button type="button">.</button>
   </div>
 );
 
-const Signs = () => (
+Numpad.propTypes = {
+  handleButtonClick: PropTypes.func.isRequired,
+};
+
+const Signs = ({ handleButtonClick }) => (
   <div className="signDiv">
-    <button type="button">+</button>
-    <button type="button">x</button>
-    <button type="button">-</button>
-    <button type="button">+</button>
-    <button type="button">=</button>
+    <button type="button" onClick={() => handleButtonClick('+')}>+</button>
+    <button type="button" onClick={() => handleButtonClick('x')}>x</button>
+    <button type="button" onClick={() => handleButtonClick('-')}>-</button>
+    <button type="button" onClick={() => handleButtonClick('+')}>+</button>
+    <button type="button" onClick={() => handleButtonClick('=')}>=</button>
   </div>
 );
 
-const Calculator = () => (
-  <div className="mainDiv">
-    <TextArea />
-    <div className="buttonDiv">
-      <Numpad />
-      <Signs />
+Signs.propTypes = {
+  handleButtonClick: PropTypes.func.isRequired,
+};
+
+const Calculator = () => {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    setCalculatorData(newData);
+  };
+
+  const handleTextChange = (event) => {
+    setCalculatorData((prevData) => ({
+      ...prevData,
+      next: event.target.value,
+    }));
+  };
+
+  return (
+    <div className="mainDiv">
+      <TextArea value={calculatorData.next} handleChange={handleTextChange} />
+      <div className="buttonDiv">
+        <Numpad handleButtonClick={handleButtonClick} />
+        <Signs handleButtonClick={handleButtonClick} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
