af0f12f9b4dddb3669eef19229f13d7a4293aa3f
Nahid1911 <nahid.raihan@gmail.com>
2023-06-09 20:39:07 +0600

    Receive API and display

---
+++ src/components/Quotes.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const FamousQuotes = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers', {
+          headers: {
+            'X-Api-Key': 'OJOHmkWoAqsbBjmeUmFoyA==yHzMMITPwxpUa3WY',
+          },
+        });
+        const data = await response.json();
+        setData(data[0]);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  if (data) {
+    return (
+      <div>
+        <div>{`"${data.quote}" -- ${data.author}`}</div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default FamousQuotes;
